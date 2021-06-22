b2a0656eccca1c0d5af16af527a20735aec8793b
Matt Carey <mcarey@west.com>
2021-06-22 15:46:15 -0600

    Added Email Sample page, new SVG, scripting and CSS tweaks

--- utility.js
+++ utility.js
@@ -57,18 +57,19 @@
           const imageAlt = data.results[0][key]['image-alt'];
 
           // Populate values from JSON into DOM
-          document.getElementById('submitSynopsis').innerHTML = synopsis;
-          document.getElementById('submitMessage').innerHTML = message;
-          document.getElementById('whatsNext').innerHTML = whatsNext;
+          if( document.getElementById('submitSynopsis') ) { document.getElementById('submitSynopsis').innerHTML = synopsis; }
+          if( document.getElementById('submitMessage') ) { document.getElementById('submitMessage').innerHTML = message; }
+          if( document.getElementById('whatsNext') ) { document.getElementById('whatsNext').innerHTML = whatsNext; }
           
-          // Assign attributes to the image and add to DOM
-          const newImage = document.createElement('object');
+          if( document.getElementById('pictureContainer') ) { 
+            // Assign attributes to the image and add to DOM
+            const newImage = document.createElement('object');
             newImage.id = "svgObject";
             newImage.className = "customSVG primarySVG";
             newImage.data = imageURL;
             newImage.type = "image/svg+xml";
             document.getElementById('pictureContainer').appendChild(newImage);
-
+          }
           // Build the 'recommendations' area from an array of items
           let content = "";
           const cardChoices = data.results[0][key]['choices'];
@@ -94,20 +95,63 @@
             }
           });
           content += "</div>"; // close out the last row
-          document.getElementById('recomendations').innerHTML = content; // insert the newly created content into the DOM
+          if(document.getElementById('recomendations')) { document.getElementById('recomendations').innerHTML = content; } // insert the newly created content into the DOM
 
         }else{ // If the specified key does not exist, fail...
           console.log("Oops...");
         }
       }
 
+      const pSBC=(p,c0,c1,l)=>{
+        //https://github.com/PimpTrizkit/PJs/wiki/12.-Shade,-Blend-and-Convert-a-Web-Color-(pSBC.js)
+        //https://jsfiddle.net/PimpTrizkit/a7ac0qvp/
+        let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
+        if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+        if(!this.pSBCr)this.pSBCr=(d)=>{
+            let n=d.length,x={};
+            if(n>9){
+                [r,g,b,a]=d=d.split(","),n=d.length;
+                if(n<3||n>4)return null;
+                x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+            }else{
+                if(n==8||n==6||n<4)return null;
+                if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+                d=i(d.slice(1),16);
+                if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
+                else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+            }return x};
+        h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=this.pSBCr(c0),P=p<0,t=c1&&c1!="c"?this.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+        if(!f||!t)return null;
+        if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+        else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+        a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+        if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+        else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+    }
+
       // Colorize SVG based on hex parameter
+function colorSVG(useColor1,useColor2){
+  const objs = document.getElementsByClassName('svgObjects');
+  for(let i=0; i<objs.length; i++){
+    const svgDoc = objs[i].contentDocument;
+    const svgItem = svgDoc.getElementsByClassName('svg-primary-color');
+    const svgItem2 = svgDoc.getElementsByClassName('svg-secondary-color');
+    for(let i=0; i<svgItem.length; i++){
+      svgItem[i].setAttribute('fill',useColor1);
+    }
+    for(let i=0; i<svgItem2.length; i++){
+      svgItem2[i].setAttribute('fill',useColor2);
+    }
+  }
+}
+
       function colorizeSvg(useColor="#582D82"){
 
         //let useColor2 = "#00B5E2";
         let useColor2 = "#ff0000";
 
         const obj = document.getElementById('svgObject');
+        if(obj!=null){
         //console.log(obj);
         const svgDoc = obj.contentDocument;
         //console.log(svgDoc);
@@ -129,7 +173,7 @@
             svgItem2[i].setAttribute('fill',useColor2);
           }
         }
-
+      }
         //svgObject.style.visibility="visible";
       }
 
@@ -193,13 +237,22 @@
         document.styleSheets[6].insertRule('a[aria-expanded="true"] { color: '+useColor+'; }', 6);
         
         const colorNoHash = useColor.split("#").join("");
-        const color40 = LightenDarkenColor(colorNoHash, +40);
-        document.styleSheets[6].insertRule('ul.submenu a { background-color: #'+color40+'; }', 6);
+        const color40 = pSBC(.40,'#'+colorNoHash); //LightenDarkenColor(colorNoHash, +40);
+        document.styleSheets[6].insertRule('ul.submenu a { background-color: '+color40+'; }', 6);
+
+        const colorLight = pSBC(.80,'#'+colorNoHash);
+        document.styleSheets[6].insertRule('.light { background-color: '+colorLight+'; }', 6);
 
         // apply border colors
         const border = document.getElementsByClassName('primaryColorBorder');
         for(let i=0; i<border.length; i++){
           border[i].style.borderColor=useColor;
+        }
+
+        //const color60 = pSBC(.60,'#'+colorNoHash); //LightenDarkenColor(colorNoHash, +80);
+        const lightBorder = document.getElementsByClassName('primaryColorBorderLight');
+        for(let i=0; i<lightBorder.length; i++){
+          lightBorder[i].style.borderColor=color40;
         }
 
         const borderBottom = document.getElementsByClassName('primaryColorBorderBottom');
@@ -264,8 +317,9 @@
 
       function updateLinks(){
         const qString = paramString
-        document.getElementById('uHome').href = "index.html"+qString;
-        document.getElementById('uRecordings').href = "recordings.html"+qString;
+        if( document.getElementById('uHome') ) { document.getElementById('uHome').href = "index.html"+qString; }
+        if( document.getElementById('uRecordings') ) { document.getElementById('uRecordings').href = "recordings.html"+qString; }
+        if( document.getElementById('uEmail') ) { document.getElementById('uEmail').href = "email.html"+qString;}
       }
 
       // Where our data/content comes from  - Use the appropriate JSON file for the language if it exists, otherwise fall back on the default
@@ -312,8 +366,18 @@
             brand = "r";
           }
 
-          document.getElementById('logo').src = data.results[0][brand]['logo'];
-          document.getElementById('footerImg').src = data.results[0][brand]['footer'];
+          if( document.getElementById('logo') ) { document.getElementById('logo').src = data.results[0][brand]['logo']; }
+          if( document.getElementById('footerImg') ) { document.getElementById('footerImg').src = data.results[0][brand]['footer']; }
+
+          const productName = document.querySelectorAll(".productName");
+          for(let i=0; i<productName.length; i++){
+            productName[i].innerHTML=data.results[0][brand]['productName'];
+          }
+
+          const companyName = document.querySelectorAll(".companyName");
+          for(let i=0; i<companyName.length; i++){
+            companyName[i].innerHTML=data.results[0][brand]['companyName'];
+          }
         };
       }
 
